0cbc60185a7f3dc558c09491b861477fbd4cb9d9
shunsuke murai <shu0603n@gmail.com>
2023-09-26 20:13:03 +0900

    SELECT

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 // material-ui
 import { Typography } from '@mui/material';
@@ -8,16 +8,72 @@ import Layout from 'layout';
 import Page from 'components/Page';
 import MainCard from 'components/MainCard';
 
+async function fetchTableData() {
+  try {
+    const response = await fetch('/api/getTable?tableName=Users');
+    if (!response.ok) {
+      throw new Error('API request failed');
+    }
+    const data = await response.json();
+    return data; // APIから返されたデータを返します
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const Top = () => (
-  <Page title="Sample Page">
-    <MainCard title="Sample Card">
-      <Typography variant="body2">TopTopTopTopTopTopTopTop</Typography>
-    </MainCard>
-  </Page>
-);
+const Top = () => {
+  const [tableData, setTableData] = useState<dbResponse>(); // データを保持する状態変数
 
+  useEffect(() => {
+    // ページがロードされたときにデータを取得
+    fetchTableData()
+      .then((data) => {
+        setTableData(data); // データを状態に設定
+      })
+      .catch((error) => {
+        // エラーハンドリング
+        console.error('Error:', error);
+      });
+  }, []); // 空の依存リストを指定することで、一度だけ実行される
+
+  type dbResponse = {
+    data: sqlResult;
+  };
+  type sqlResult = {
+    command: string;
+    fields: Array<any>;
+    rowAsArray: boolean;
+    rowCount: number;
+    rows: Array<Usres>;
+    viaNeonFetch: boolean;
+  };
+
+  type Usres = {
+    user_id: string;
+    email: string;
+    password: string;
+  };
+
+  return (
+    <Page title="Sample Page">
+      <MainCard title="Sample Card">
+        <Typography variant="body2">
+          {/* データをマップして表示 */}
+          <p>-----------Usersテーブル------------</p>
+          {tableData?.data.rows.map((item) => (
+            // <div key={item.user_id}>{item.email}</div>
+            <p key={item.user_id}>
+              {item.user_id} {item.email} {item.password}
+            </p>
+          ))}
+        </Typography>
+      </MainCard>
+    </Page>
+  );
+};
 Top.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
