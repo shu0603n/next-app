c762271d1b76228acb8b6ead9b9121c8f1c59cfd
shunsuke murai <shu0603n@gmail.com>
2023-09-26 20:32:39 +0900

    パラメータ追加

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -1,6 +1,7 @@
 // project import
 import other from './other';
 import pages from './pages';
+import parameter from './parameter';
 
 //types
 import { NavItemType } from 'types/menu';
@@ -8,7 +9,7 @@ import { NavItemType } from 'types/menu';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems: { items: NavItemType[] } = {
-  items: [pages, other]
+  items: [pages, other, parameter]
 };
 
 export default menuItems;
