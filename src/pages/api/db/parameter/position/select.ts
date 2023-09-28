c25fe8dddf22d3ab0ea16247853cba9977867410
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-28 16:53:49 +0900

    Update select.ts

--- src/pages/api/db/parameter/position/select.ts
+++ src/pages/api/db/parameter/position/select.ts
@@ -3,10 +3,7 @@ import { NextApiResponse, NextApiRequest } from 'next';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   try {
-    const tableName = request.query.tableName as string;
-    console.log(tableName);
     const data = await sql`SELECT * FROM position;`;
-    console.log(data);
     return response.status(200).json({ data });
   } catch (error) {
     console.error('エラーが発生しました:', error);
