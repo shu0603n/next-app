5f8d9315ce94a7519359bbc85c3b43eb6c32cb87
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-28 16:53:39 +0900

    Update insert.ts

--- src/pages/api/db/parameter/position/insert.ts
+++ src/pages/api/db/parameter/position/insert.ts
@@ -6,10 +6,12 @@ export default async function handler(request: NextApiRequest, response: NextApi
     const name = request.query.name as string;
     if (!name) throw new Error('パラメーターが不足しています');
     await sql`INSERT INTO Position (Name) VALUES (${name});`;
-  } catch (error) {
-    return response.status(500).json({ error });
-  }
 
-  const data = await sql`SELECT * FROM Position;`;
-  return response.status(200).json({ data });
+    const data = await sql`SELECT * FROM position;`;
+    console.log(data);
+    return response.status(200).json({ data });
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+    return response.status(500).json({ error: 'データを取得できませんでした。' });
+  }
 }
