088b0aa8b4289af2a5025a12c1f519136394d7d2
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-29 14:53:41 +0900

    Create AlertCustomerDelete.tsx

---
+++ src/sections/apps/employee/skill-table/AlertCustomerDelete.tsx
@@ -0,0 +1,62 @@
+// material-ui
+import { Button, Dialog, DialogContent, Stack, Typography } from '@mui/material';
+
+// project import
+import Avatar from 'components/@extended/Avatar';
+import { PopupTransition } from 'components/@extended/Transitions';
+
+// assets
+import { DeleteFilled } from '@ant-design/icons';
+
+// types
+interface Props {
+  title: string;
+  open: boolean;
+  handleClose: (status: boolean) => void;
+}
+
+// ==============================|| 顧客 - 削除 ||============================== //
+
+export default function AlertCustomerDelete({ title, open, handleClose }: Props) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => handleClose(false)}
+      keepMounted
+      TransitionComponent={PopupTransition}
+      maxWidth="xs"
+      aria-labelledby="column-delete-title"
+      aria-describedby="column-delete-description"
+    >
+      <DialogContent sx={{ mt: 2, my: 1 }}>
+        <Stack alignItems="center" spacing={3.5}>
+          <Avatar color="error" sx={{ width: 72, height: 72, fontSize: '1.75rem' }}>
+            <DeleteFilled />
+          </Avatar>
+          <Stack spacing={2}>
+            <Typography variant="h4" align="center">
+              削除してもよろしいですか？
+            </Typography>
+            <Typography align="center">
+              「
+              <Typography variant="subtitle1" component="span">
+                {' '}
+                {title}{' '}
+              </Typography>
+              」ユーザーを削除すると、そのユーザーに割り当てられたすべてのタスクも削除されます。
+            </Typography>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{ width: 1 }}>
+            <Button fullWidth onClick={() => handleClose(false)} color="secondary" variant="outlined">
+              キャンセル
+            </Button>
+            <Button fullWidth color="error" variant="contained" onClick={() => handleClose(true)} autoFocus>
+              削除
+            </Button>
+          </Stack>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+}
