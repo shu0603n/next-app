c506681e0296c6ba3b61a8226c8b6e70043132b1
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-29 14:50:53 +0900

    Create [tab].tsx

---
+++ src/pages/employee/[tab].tsx
@@ -0,0 +1,69 @@
+import { useState, ReactElement } from 'react';
+
+// next
+import { useRouter } from 'next/router';
+
+// material-ui
+import { Box, Tab, Tabs } from '@mui/material';
+
+// project import
+import Layout from 'layout';
+import Page from 'components/Page';
+import MainCard from 'components/MainCard';
+import TabProfile from 'sections/apps/employee/TabProfile';
+import TabPersonal from 'sections/apps/employee/TabPersonal';
+import TabSkill from 'sections/apps/employee/TabSkill';
+import TabAccount from 'sections/apps/employee/TabAccount';
+import TabPassword from 'sections/apps/employee/TabPassword';
+import TabRole from 'sections/apps/employee/TabRole';
+import TabSettings from 'sections/apps/employee/TabSettings';
+
+// assets
+import { ContainerOutlined, FileTextOutlined, LockOutlined, SettingOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+
+// ==============================|| PROFILE - ACCOUNT ||============================== //
+
+const EmployeeDetail = () => {
+  const router = useRouter();
+  const { tab } = router.query;
+
+  const [value, setValue] = useState(tab);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    router.push(`/employee/${newValue}`);
+  };
+
+  return (
+    <Page title="Account Profile">
+      <MainCard border={false} boxShadow>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%' }}>
+          <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="auto" aria-label="account profile tab">
+            <Tab label="Profile" icon={<UserOutlined />} value="basic" iconPosition="start" />
+            <Tab label="Personal" icon={<FileTextOutlined />} value="personal" iconPosition="start" />
+            <Tab label="skill" icon={<FileTextOutlined />} value="skill" iconPosition="start" />
+            <Tab label="My Account" icon={<ContainerOutlined />} value="my-account" iconPosition="start" />
+            <Tab label="Change Password" icon={<LockOutlined />} value="password" iconPosition="start" />
+            <Tab label="Role" icon={<TeamOutlined />} value="role" iconPosition="start" />
+            <Tab label="Settings" icon={<SettingOutlined />} value="settings" iconPosition="start" />
+          </Tabs>
+        </Box>
+        <Box sx={{ mt: 2.5 }}>
+          {tab === 'basic' && <TabProfile />}
+          {tab === 'personal' && <TabPersonal />}
+          {tab === 'skill' && <TabSkill />}
+          {tab === 'my-account' && <TabAccount />}
+          {tab === 'password' && <TabPassword />}
+          {tab === 'role' && <TabRole />}
+          {tab === 'settings' && <TabSettings />}
+        </Box>
+      </MainCard>
+    </Page>
+  );
+};
+
+EmployeeDetail.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default EmployeeDetail;
