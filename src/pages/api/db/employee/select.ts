b3bbd317642fa77a9b7af6ce31a848fb9b6a71ba
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-09-29 15:20:23 +0900

    Create select.ts

---
+++ src/pages/api/db/employee/select.ts
@@ -0,0 +1,25 @@
+import { sql } from '@vercel/postgres';
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const data = await sql`
+    SELECT 
+        emp.id,
+        emp.sei || ' ' || emp.mei AS name,
+        emp.sei_k || ' ' || emp.mei_k AS full_name_k,
+        emp.gender,
+        Job_category.name AS job_category_name,
+        Employment.name AS employment_name
+    FROM employee AS emp
+    INNER JOIN job_category 
+        ON emp.job_category_id = job_category.id
+    INNER JOIN employment
+        ON emp.employment_id = employment.id;
+    `;
+    return response.status(200).json({ data });
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+    return response.status(500).json({ error: 'データを取得できませんでした。' });
+  }
+}
