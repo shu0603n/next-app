2d39366faa24f388deb48cc0fbc3dbca0d1c6af3
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-05 16:31:26 +0900

    Update TabSkill.tsx

--- src/sections/apps/employee/TabSkill.tsx
+++ src/sections/apps/employee/TabSkill.tsx
@@ -1,54 +1,74 @@
 // material-ui
 import { Grid } from '@mui/material';
 import SkillTable from './skill-table/SkillTable';
+import { useState, useEffect } from 'react';
+import { SkillTableType } from 'types/employee/skill-table';
 
 // アセット
 
 // ==============================|| アカウントプロファイル - 役割 ||============================== //
 
-const TabRole = () => {
-  const skill = [
-    {
-      experience_years: 15,
-      technic_name: '言語',
-      skill_name: 'JAVA'
-    },
-    {
-      experience_years: 4,
-      technic_name: '言語',
-      skill_name: 'Ptyhon'
-    },
-    {
-      experience_years: 0,
-      technic_name: '言語',
-      skill_name: 'TypeScript'
-    },
-    {
-      experience_years: 1.5,
-      technic_name: 'DataBase',
-      skill_name: 'Oracle'
-    },
-    {
-      experience_years: 5,
-      technic_name: 'DataBase',
-      skill_name: 'postgreSQL'
-    },
-    {
-      experience_years: 1.5,
-      technic_name: 'library',
-      skill_name: 'React'
-    },
-    {
-      experience_years: 10,
-      technic_name: 'library',
-      skill_name: 'Flask'
+async function fetchSkillList(id: number) {
+  try {
+    const response = await fetch(`/api/db/employee/skill/skills/select?id=${id}`);
+    if (!response.ok) {
+      throw new Error('API request failed');
     }
-  ];
-  console.log(skill);
+    const data = await response.json();
+    return data; // APIから返されたデータを返します
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+async function fetchTableData() {
+  try {
+    const response = await fetch(`/api/db/employee/skill/select?id=${1}`);
+    if (!response.ok) {
+      throw new Error('API request failed');
+    }
+    const data = await response.json();
+
+    // マップ処理を非同期関数として実行し、Promise.allで待機します
+    data.data.rows = await Promise.all(
+      data.data.rows.map(async (row: SkillTableType) => {
+        try {
+          const skills = await fetchSkillList(row.skills_used_id);
+          return { ...row, skill: skills.data.rows ?? [] };
+        } catch (error) {
+          console.error('Error fetching skills:', error);
+          return { ...row, skill: [] };
+        }
+      })
+    );
+
+    return data; // APIから返されたデータを返します
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+const TabRole = () => {
+  const [data, setData] = useState<SkillTableType[]>([]); // デフォルト値を空の配列に設定
+
+  useEffect(() => {
+    // ページがロードされたときにデータを取得
+    fetchTableData()
+      .then((fetchedData) => {
+        setData(fetchedData.data.rows);
+      })
+      .catch((error) => {
+        // エラーハンドリング
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <SkillTable skill={skill} />
+        {data.length > 0 && <SkillTable data={data} />}
       </Grid>
     </Grid>
   );
