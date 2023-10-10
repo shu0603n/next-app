d98dd90af8bc6782adf14155f2a7b74dc8216091
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-10 14:14:40 +0900

    Create client.ts

---
+++ src/types/client/client.ts
@@ -0,0 +1,10 @@
+export type ClientType = {
+  id: number;
+  name: string;
+  name_k: string;
+  phone: string;
+  email: string;
+  address: string;
+  postal_code: string;
+  remarks: string;
+};
