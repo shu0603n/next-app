4de2dc291b146c65f53c1ab42a426450a172a969
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-10 14:14:58 +0900

    Create project.ts

---
+++ src/types/project/project.ts
@@ -0,0 +1,15 @@
+export type ProjectType = {
+  id: number;
+  hp_posting_flag: boolean;
+  client_id: number;
+  client_name: string;
+  contract_id: number;
+  contract_name: string;
+  working_postal_code: string;
+  working_address: string;
+  working_start_time: string;
+  working_end_time: string;
+  holiday: string;
+  project_title: string;
+  business_outline: string;
+};
