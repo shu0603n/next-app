cf16a718cbc4535e8e69564460df6a53dc142126
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-10 14:12:03 +0900

    Create TabPassword.tsx

---
+++ src/sections/apps/client/TabPassword.tsx
@@ -0,0 +1,263 @@
+import { useState, SyntheticEvent } from 'react';
+
+// material-ui
+import {
+  Box,
+  Button,
+  FormHelperText,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  OutlinedInput,
+  Stack,
+  Typography
+} from '@mui/material';
+
+// project import
+import MainCard from 'components/MainCard';
+import IconButton from 'components/@extended/IconButton';
+
+import { dispatch } from 'store';
+import { openSnackbar } from 'store/reducers/snackbar';
+import { isNumber, isLowercaseChar, isUppercaseChar, isSpecialChar, minLength } from 'utils/password-validation';
+
+// third party
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
+// assets
+import { CheckOutlined, EyeOutlined, EyeInvisibleOutlined, LineOutlined } from '@ant-design/icons';
+
+// ==============================|| TAB - PASSWORD CHANGE ||============================== //
+
+const TabPassword = () => {
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleClickShowOldPassword = () => {
+    setShowOldPassword(!showOldPassword);
+  };
+  const handleClickShowNewPassword = () => {
+    setShowNewPassword(!showNewPassword);
+  };
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const handleMouseDownPassword = (event: SyntheticEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <MainCard title="パスワード変更">
+      <Formik
+        initialValues={{
+          old: '',
+          password: '',
+          confirm: '',
+          submit: null
+        }}
+        validationSchema={Yup.object().shape({
+          old: Yup.string().required('古いパスワードは必須です'),
+          password: Yup.string()
+            .required('新しいパスワードは必須です')
+            .matches(
+              /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+              'パスワードは少なくとも8文字、大文字1文字、数字1文字、特殊文字1文字を含む必要があります'
+            ),
+          confirm: Yup.string()
+            .required('新しいパスワードの確認は必須です')
+            .test('confirm', 'パスワードが一致しません', (confirm, yup) => yup.parent.password === confirm)
+        })}
+        onSubmit={async (values, { resetForm, setErrors, setStatus, setSubmitting }) => {
+          try {
+            dispatch(
+              openSnackbar({
+                open: true,
+                message: 'パスワードが正常に変更されました。',
+                variant: 'alert',
+                alert: {
+                  color: 'success'
+                },
+                close: false
+              })
+            );
+
+            resetForm();
+            setStatus({ success: false });
+            setSubmitting(false);
+          } catch (err: any) {
+            setStatus({ success: false });
+            setErrors({ submit: err.message });
+            setSubmitting(false);
+          }
+        }}
+      >
+        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+          <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item container spacing={3} xs={12} sm={6}>
+                <Grid item xs={12}>
+                  <Stack spacing={1.25}>
+                    <InputLabel htmlFor="password-old">古いパスワード</InputLabel>
+                    <OutlinedInput
+                      id="password-old"
+                      placeholder="古いパスワードを入力"
+                      type={showOldPassword ? 'text' : 'password'}
+                      value={values.old}
+                      name="old"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="パスワードの表示切り替え"
+                            onClick={handleClickShowOldPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="large"
+                            color="secondary"
+                          >
+                            {showOldPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      inputProps={{}}
+                    />
+                    {touched.old && errors.old && (
+                      <FormHelperText error id="password-old-helper">
+                        {errors.old}
+                      </FormHelperText>
+                    )}
+                  </Stack>
+                </Grid>
+                <Grid item xs={12}>
+                  <Stack spacing={1.25}>
+                    <InputLabel htmlFor="password-password">新しいパスワード</InputLabel>
+                    <OutlinedInput
+                      id="password-password"
+                      placeholder="新しいパスワードを入力"
+                      type={showNewPassword ? 'text' : 'password'}
+                      value={values.password}
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="パスワードの表示切り替え"
+                            onClick={handleClickShowNewPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="large"
+                            color="secondary"
+                          >
+                            {showNewPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      inputProps={{}}
+                    />
+                    {touched.password && errors.password && (
+                      <FormHelperText error id="password-password-helper">
+                        {errors.password}
+                      </FormHelperText>
+                    )}
+                  </Stack>
+                </Grid>
+                <Grid item xs={12}>
+                  <Stack spacing={1.25}>
+                    <InputLabel htmlFor="password-confirm">新しいパスワード（確認）</InputLabel>
+                    <OutlinedInput
+                      id="password-confirm"
+                      placeholder="新しいパスワードを再度入力"
+                      type={showConfirmPassword ? 'text' : 'password'}
+                      value={values.confirm}
+                      name="confirm"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="パスワードの表示切り替え"
+                            onClick={handleClickShowConfirmPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="large"
+                            color="secondary"
+                          >
+                            {showConfirmPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      inputProps={{}}
+                    />
+                    {touched.confirm && errors.confirm && (
+                      <FormHelperText error id="password-confirm-helper">
+                        {errors.confirm}
+                      </FormHelperText>
+                    )}
+                  </Stack>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Box sx={{ p: { xs: 0, sm: 2, md: 4, lg: 5 } }}>
+                  <Typography variant="h5">新しいパスワードは以下を含む必要があります:</Typography>
+                  <List sx={{ p: 0, mt: 1 }}>
+                    <ListItem divider>
+                      <ListItemIcon sx={{ color: minLength(values.password) ? 'success.main' : 'inherit' }}>
+                        {minLength(values.password) ? <CheckOutlined /> : <LineOutlined />}
+                      </ListItemIcon>
+                      <ListItemText primary="少なくとも8文字" />
+                    </ListItem>
+                    <ListItem divider>
+                      <ListItemIcon sx={{ color: isLowercaseChar(values.password) ? 'success.main' : 'inherit' }}>
+                        {isLowercaseChar(values.password) ? <CheckOutlined /> : <LineOutlined />}
+                      </ListItemIcon>
+                      <ListItemText primary="少なくとも1つの小文字 (a-z)" />
+                    </ListItem>
+                    <ListItem divider>
+                      <ListItemIcon sx={{ color: isUppercaseChar(values.password) ? 'success.main' : 'inherit' }}>
+                        {isUppercaseChar(values.password) ? <CheckOutlined /> : <LineOutlined />}
+                      </ListItemIcon>
+                      <ListItemText primary="少なくとも1つの大文字 (A-Z)" />
+                    </ListItem>
+                    <ListItem divider>
+                      <ListItemIcon sx={{ color: isNumber(values.password) ? 'success.main' : 'inherit' }}>
+                        {isNumber(values.password) ? <CheckOutlined /> : <LineOutlined />}
+                      </ListItemIcon>
+                      <ListItemText primary="少なくとも1つの数字 (0-9)" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon sx={{ color: isSpecialChar(values.password) ? 'success.main' : 'inherit' }}>
+                        {isSpecialChar(values.password) ? <CheckOutlined /> : <LineOutlined />}
+                      </ListItemIcon>
+                      <ListItemText primary="少なくとも1つの特殊文字" />
+                    </ListItem>
+                  </List>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={2}>
+                  <Button variant="outlined" color="secondary">
+                    キャンセル
+                  </Button>
+                  <Button disabled={isSubmitting || Object.keys(errors).length !== 0} type="submit" variant="contained">
+                    プロフィールを更新
+                  </Button>
+                </Stack>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+    </MainCard>
+  );
+};
+
+export default TabPassword;
