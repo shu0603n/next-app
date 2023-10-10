86984771d26bf387ab033c95f18af634f78dce72
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-10 14:09:02 +0900

    Create select.ts

---
+++ src/pages/api/db/project/select.ts
@@ -0,0 +1,28 @@
+import { sql } from '@vercel/postgres';
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const data = await sql`
+    SELECT
+      project.id,
+      project.hp_posting_flag,
+      client.name AS client_Name,
+      contract.name AS contract_Name,
+      project.working_postal_code,
+      project.working_address,
+      project.working_start_time,
+      project.working_end_time,
+      project.holiday,
+      project.project_title,
+      project.business_outline
+  FROM project
+  LEFT JOIN client ON project.client_id = client.id
+  LEFT JOIN contract ON project.contract_id = contract.id;
+    `;
+    return response.status(200).json({ data });
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+    return response.status(500).json({ error: 'データを取得できませんでした。' });
+  }
+}
