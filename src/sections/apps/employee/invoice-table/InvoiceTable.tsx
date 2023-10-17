f4722969534d67c530005d7bb44a4712970000c2
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-17 15:24:38 +0900

    Create InvoiceTable.tsx

---
+++ src/sections/apps/employee/invoice-table/InvoiceTable.tsx
@@ -0,0 +1,317 @@
+import { useCallback, useEffect, useMemo, useState, FC, Fragment, MouseEvent } from 'react';
+
+// material-ui
+import { alpha, useTheme } from '@mui/material/styles';
+import { Button, Dialog, Stack, Table, TableBody, TableCell, TableHead, TableRow, Tooltip, useMediaQuery } from '@mui/material';
+
+// third-party
+import {
+  useFilters,
+  useExpanded,
+  useGlobalFilter,
+  useRowSelect,
+  useSortBy,
+  useTable,
+  usePagination,
+  Column,
+  HeaderGroup,
+  Row,
+  Cell,
+  HeaderProps
+} from 'react-table';
+
+// project import
+import MainCard from 'components/MainCard';
+import ScrollX from 'components/ScrollX';
+import IconButton from 'components/@extended/IconButton';
+import { PopupTransition } from 'components/@extended/Transitions';
+import {
+  CSVExport,
+  HeaderSort,
+  IndeterminateCheckbox,
+  SortingSelect,
+  TablePagination,
+  TableRowSelection
+} from 'components/third-party/ReactTable';
+
+import AddCustomer from 'sections/apps/employee/invoice-table/AddCustomer';
+import CustomerView from 'sections/apps/employee/invoice-table/CustomerView';
+import AlertCustomerDelete from 'sections/apps/employee/invoice-table/AlertCustomerDelete';
+
+// import makeData from 'data/react-table';
+import { renderFilterTypes, GlobalFilter } from 'utils/react-table';
+
+// assets
+import { CloseOutlined, PlusOutlined, EyeTwoTone, EditTwoTone, DeleteTwoTone } from '@ant-design/icons';
+import { InvoiceTableType } from 'types/employee/invoice-table';
+
+// ==============================|| REACT TABLE - EDITABLE ROW ||============================== //
+
+interface Props {
+  columns: Column[];
+  data: InvoiceTableType[];
+  handleAdd: () => void;
+  renderRowSubComponent: FC<any>;
+  getHeaderProps: (column: HeaderGroup) => {};
+}
+
+function ReactTable({ columns, data, renderRowSubComponent, handleAdd, getHeaderProps }: Props) {
+  const theme = useTheme();
+  const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const filterTypes = useMemo(() => renderFilterTypes, []);
+  const sortBy = { id: 'project_name', desc: false };
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    setHiddenColumns,
+    allColumns,
+    visibleColumns,
+    rows,
+    page,
+    gotoPage,
+    setPageSize,
+    state: { globalFilter, selectedRowIds, pageIndex, pageSize, expanded },
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    setSortBy,
+    selectedFlatRows
+  } = useTable(
+    {
+      columns,
+      data,
+      filterTypes,
+      initialState: { pageIndex: 0, pageSize: 10, hiddenColumns: ['cliant'], sortBy: [sortBy] }
+    },
+    useGlobalFilter,
+    useFilters,
+    useSortBy,
+    useExpanded,
+    usePagination,
+    useRowSelect
+  );
+
+  useEffect(() => {
+    if (matchDownSM) {
+      setHiddenColumns(['start_date', 'client_name', 'people_number']);
+    } else {
+      setHiddenColumns(['people_number']);
+    }
+    // eslint-disable-next-line
+  }, [matchDownSM]);
+
+  return (
+    <Fragment>
+      <TableRowSelection selected={Object.keys(selectedRowIds).length} />
+      <Stack spacing={3}>
+        <Stack
+          direction={matchDownSM ? 'column' : 'row'}
+          spacing={1}
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ p: 3, pb: 0 }}
+        >
+          <GlobalFilter
+            preGlobalFilteredRows={preGlobalFilteredRows}
+            globalFilter={globalFilter}
+            setGlobalFilter={setGlobalFilter}
+            size="small"
+          />
+          <Stack direction={matchDownSM ? 'column' : 'row'} alignItems="center" spacing={1}>
+            <SortingSelect sortBy={sortBy.id} setSortBy={setSortBy} allColumns={allColumns} />
+            <Button variant="contained" startIcon={<PlusOutlined />} onClick={handleAdd} size="small">
+              新規追加
+            </Button>
+            <CSVExport
+              data={selectedFlatRows.length > 0 ? selectedFlatRows.map((d: Row) => d.original) : data}
+              filename={'customer-list.csv'}
+            />
+          </Stack>
+        </Stack>
+        <Table {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map((headerGroup: HeaderGroup<{}>) => (
+              // eslint-disable-next-line react/jsx-key
+              <TableRow {...headerGroup.getHeaderGroupProps()} sx={{ '& > th:first-of-type': { width: '58px' } }}>
+                {headerGroup.headers.map((column: HeaderGroup) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <TableCell {...column.getHeaderProps([{ className: column.className }, getHeaderProps(column)])}>
+                    <HeaderSort column={column} />
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody {...getTableBodyProps()}>
+            {page.map((row: Row, i: number) => {
+              prepareRow(row);
+              const rowProps = row.getRowProps();
+
+              return (
+                <Fragment key={i}>
+                  <TableRow
+                    {...row.getRowProps()}
+                    onClick={() => {
+                      row.toggleRowSelected();
+                    }}
+                    sx={{ cursor: 'pointer', bgcolor: row.isSelected ? alpha(theme.palette.primary.lighter, 0.35) : 'inherit' }}
+                  >
+                    {row.cells.map((cell: Cell) => (
+                      // eslint-disable-next-line react/jsx-key
+                      <TableCell {...cell.getCellProps([{ className: cell.column.className }])}>{cell.render('Cell')}</TableCell>
+                    ))}
+                  </TableRow>
+                  {row.isExpanded && renderRowSubComponent({ row, rowProps, visibleColumns, expanded })}
+                </Fragment>
+              );
+            })}
+            <TableRow sx={{ '&:hover': { bgcolor: 'transparent !important' } }}>
+              <TableCell sx={{ p: 2, py: 3 }} colSpan={9}>
+                <TablePagination gotoPage={gotoPage} rows={rows} setPageSize={setPageSize} pageSize={pageSize} pageIndex={pageIndex} />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Stack>
+    </Fragment>
+  );
+}
+
+interface InvoiceTableProps {
+  data: InvoiceTableType[];
+}
+// ==============================|| CUSTOMER - LIST ||============================== //
+const InvoiceTable = ({ data }: InvoiceTableProps) => {
+  const theme = useTheme();
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [customer, setCustomer] = useState<any>(null);
+  const [customerDeleteId, setCustomerDeleteId] = useState<any>('');
+  const [add, setAdd] = useState<boolean>(false);
+
+  const handleAdd = () => {
+    setAdd(!add);
+    if (customer && !add) setCustomer(null);
+  };
+
+  const handleClose = () => {
+    setOpen(!open);
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        title: 'Row Selection',
+        Header: ({ getToggleAllPageRowsSelectedProps }: HeaderProps<{}>) => (
+          <IndeterminateCheckbox indeterminate {...getToggleAllPageRowsSelectedProps()} />
+        ),
+        accessor: 'selection',
+        Cell: ({ row }: any) => <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />,
+        disableSortBy: true
+      },
+      {
+        Header: '請求日',
+        accessor: 'invoice_date'
+      },
+      {
+        Header: 'プロジェクト名',
+        accessor: 'project_name'
+      },
+      {
+        Header: '企業名',
+        accessor: 'client_name'
+      },
+      {
+        Header: '請求金額',
+        accessor: 'invoice_amount'
+      },
+      {
+        Header: 'アクション',
+        className: 'cell-center',
+        disableSortBy: true,
+        Cell: ({ row }: { row: Row<{}> }) => {
+          const collapseIcon = row.isExpanded ? (
+            <CloseOutlined style={{ color: theme.palette.error.main }} />
+          ) : (
+            <EyeTwoTone twoToneColor={theme.palette.secondary.main} />
+          );
+          return (
+            <Stack direction="row" alignItems="center" justifyContent="center" spacing={0}>
+              <Tooltip title="View">
+                <IconButton
+                  color="secondary"
+                  onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                    e.stopPropagation();
+                    row.toggleRowExpanded();
+                  }}
+                >
+                  {collapseIcon}
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Edit">
+                <IconButton
+                  color="primary"
+                  onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                    e.stopPropagation();
+                    setCustomer(row.values);
+                    handleAdd();
+                  }}
+                >
+                  <EditTwoTone twoToneColor={theme.palette.primary.main} />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Delete">
+                <IconButton
+                  color="error"
+                  onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                    e.stopPropagation();
+                    handleClose();
+                    setCustomerDeleteId(row.values.id);
+                  }}
+                >
+                  <DeleteTwoTone twoToneColor={theme.palette.error.main} />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+          );
+        }
+      }
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [theme]
+  );
+
+  const renderRowSubComponent = useCallback(({ row }: { row: Row<{}> }) => <CustomerView data={data[Number(row.id)]} />, [data]);
+  return (
+    <MainCard content={false}>
+      <ScrollX>
+        <ReactTable
+          columns={columns}
+          data={data}
+          handleAdd={handleAdd}
+          renderRowSubComponent={renderRowSubComponent}
+          getHeaderProps={(column: HeaderGroup) => column.getSortByToggleProps()}
+        />
+      </ScrollX>
+      <AlertCustomerDelete title={customerDeleteId} open={open} handleClose={handleClose} />
+      {/* add customer dialog */}
+      <Dialog
+        maxWidth="sm"
+        TransitionComponent={PopupTransition}
+        keepMounted
+        fullWidth
+        onClose={handleAdd}
+        open={add}
+        sx={{ '& .MuiDialog-paper': { p: 0 }, transition: 'transform 225ms' }}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <AddCustomer customer={customer} onCancel={handleAdd} />
+      </Dialog>
+    </MainCard>
+  );
+};
+
+export default InvoiceTable;
