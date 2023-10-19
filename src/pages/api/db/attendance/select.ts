9c33802fca943575b904c40706684a4425c2dcf8
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-19 09:17:45 +0900

    Update select.ts

--- src/pages/api/db/attendance/select.ts
+++ src/pages/api/db/attendance/select.ts
@@ -3,46 +3,10 @@ import { NextApiResponse, NextApiRequest } from 'next';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   try {
-    const toNull = (str: string) => {
-      return str === null || str === '' ? null : str;
-    };
-    const { employee_id, date, start_time, end_time, location } = request.body;
+    const { employee_id, date } = request.body;
     const dateFormat = new Date(date);
     const year = dateFormat.getFullYear(); // 年を取得
     const month = dateFormat.getMonth() + 1; // 月を取得 (0から始まるので+1)
-
-    if (!employee_id || !date) {
-      throw new Error('パラメーターが不足しています');
-    }
-
-    // 既存のデータをUPDATE
-    const updateResult = await sql`
-      UPDATE attendance
-      SET
-        start_time = ${toNull(start_time)},
-        end_time = ${toNull(end_time)},
-        location = ${toNull(location)}
-      WHERE
-        employee_id = ${Number(employee_id)} AND
-        date = ${toNull(date)}
-    `;
-
-    if (updateResult.rowCount === 0) {
-      // IDが存在しない場合、新しいレコードをINSERT
-      const insertResult = await sql`
-        INSERT INTO attendance (employee_id, date, start_time, end_time, location)
-        VALUES (
-          ${Number(employee_id)},
-          ${toNull(date)},
-          ${toNull(start_time)},
-          ${toNull(end_time)},
-          ${toNull(location)}
-        )
-      `;
-      if (insertResult.rowCount !== 1) {
-        throw new Error('データの挿入中に問題が発生しました');
-      }
-    }
 
     // データを取得
 
