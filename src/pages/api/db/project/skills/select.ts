f4f54ef98f5fc9a50749587af2bd43fa65f70352
shu0603n <61679407+shu0603n@users.noreply.github.com>
2023-10-24 13:29:42 +0900

    Create select.ts

---
+++ src/pages/api/db/project/skills/select.ts
@@ -0,0 +1,31 @@
+import { sql } from '@vercel/postgres';
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const id = request.query.id as string;
+    const data = await sql`
+      SELECT
+          skill.id,
+          skill.name,
+          technic.name AS technic_name,
+          skill.candidate_flag 
+      FROM
+          skills_used
+      INNER JOIN
+          skill
+      ON
+          skills_used.Skill_id = skill.id
+      LEFT JOIN
+          technic
+      ON
+          skill.Technic_id = technic.id
+      WHERE
+          skills_used.employee_skills_id = ${Number(id)}
+    `;
+    return response.status(200).json({ data });
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+    return response.status(500).json({ error: 'データを取得できませんでした。' });
+  }
+}
